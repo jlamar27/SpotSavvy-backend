da13699a82095d7ef647785966b4ea85a40d0117
zubinsood <zubinsood@icloud.com>
2023-10-20 10:39:58 -0400

    auth works

--- spotsavvy/settings.py
+++ spotsavvy/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY=env('SECRET_KEY')
-DATABASE_URL=env('DATABASE_URL')
+# DATABASE_URL=env('DATABASE_URL')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,21 +98,21 @@ WSGI_APPLICATION = 'spotsavvy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'testsavvy',
-#         'USER': 'test',
-#         'PASSWORD': 'test',
-#         'HOST': 'localhost',  # Set to your database server's address if it's not on your local machine
-#         'PORT': '5432',       # The default postgres port is 5432
-#     }
-# }
-
 DATABASES = {
-    'default': 
-        dj_database_url.config('DATABASE_URL')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testsavvy',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',  # Set to your database server's address if it's not on your local machine
+        'PORT': '5432',       # The default postgres port is 5432
+    }
 }
+
+# DATABASES = {
+#     'default': 
+#         dj_database_url.config('DATABASE_URL')
+# }
 
 # DATABASES = {
 #     'default': {
@@ -178,4 +178,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
